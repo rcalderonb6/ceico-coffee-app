eeb15d991fd5553c47b0c9d4e81e98cb0a5ffb36
Rodrigo Calderón <92266588+rcalderonb6@users.noreply.github.com>
2024-11-29 00:19:41 +0100

    Update daily.py

--- daily.py
+++ daily.py
@@ -7,7 +7,13 @@ chart_data=pd.DataFrame(np.random.randn(20,3),columns=["a","b","c"])
 
 employees=['Øyvind','Rodrigo','Valentina','Fede','Iggy']
 
+coffees_per_kg=0
+
 with st.sidebar:
-  st.header("Input Coffee Consumption")
+  st.header("Add a coffee!☕")
   name=st.selectbox('Name',employees)
+  last_coffee=st.selectbox('What is the last coffee?',['yes','no'])
   
+  if last_coffee:
+    price_per_cup=compute_price_per_cup(number_of_cups,price_kg)
+    coffees_per_kg
